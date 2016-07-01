a0b923b998d823db78f2ef5f310a4f1d36807ef0
Sandeep Jangra <sjangra@walmartlabs.com>
2016-06-30 19:01:14 -0700

    update

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,14 +1,26 @@
 #!/bin/bash
 
+if [ "$#" -ne 2 ]; then
+  echo "Usage: ./test.sh <IP-Address-MariaDB-Server-1> <IP-Address-MariaDB-Server-2>"
+  echo "We will be writing to server-1 and checking if replication is synchronous on server-2" 
+  exit 1
+fi
+
 SOURCE=$1
 REPLICA=$2
+## Value of wsrep_sync_wait variable
 WSREP_SYNC_VAL=1
 
+###################################################
+### Create test table ############################
+###################################################
 mysql -h $SOURCE -u root -ppass123 -e "CREATE TABLE test.t1 (id INT PRIMARY KEY AUTO_INCREMENT) ENGINE=InnoDB;commit;"
 mysql -h $SOURCE -u root -ppass123 -e "INSERT INTO test.t1 VALUES (DEFAULT);commit;"
 
-#let $qcache_size_orig = `SELECT @@GLOBAL.query_cache_size`
-#mysql -u root -ppass123 -e "SET GLOBAL query_cache_size=1355776;"
+##let $qcache_size_orig = `SELECT @@GLOBAL.query_cache_size`
+mysql -h $SOURCE -u root -ppass123 -e "SET GLOBAL query_cache_size=1355776;"
+
+###### Set wsrep_sync_wait variable at GLOBAL level #########
 mysql -h $SOURCE -u root -ppass123 -e "SET GLOBAL wsrep_sync_wait = $WSREP_SYNC_VAL;"
 mysql -h $REPLICA -u root -ppass123 -e "SET GLOBAL wsrep_sync_wait = $WSREP_SYNC_VAL;"
 
@@ -16,12 +28,12 @@ mysql -h $REPLICA -u root -ppass123 -e "SET GLOBAL wsrep_sync_wait = $WSREP_SYNC
 count=1
 while [ "$count" -le 5000 ]
 do
-  ##connection node_1
+  ##connection node_1: Connect to first node and insert a row
   mysql -h $SOURCE -u root -ppass123 -e "INSERT INTO test.t1 VALUES (DEFAULT);commit;"
   mysql -h $SOURCE -u root -ppass123 -e "SET SESSION wsrep_sync_wait = $WSREP_SYNC_VAL; SELECT MAX(id) FROM test.t1;" > /tmp/f1
   val1=`tail -1 /tmp/f1`
 
-  ##connection node_2
+  ##connection node_2: connect to second node and read from it
   mysql -h $REPLICA -u root -ppass123 -e "SET SESSION wsrep_sync_wait = $WSREP_SYNC_VAL; SELECT MAX(id) FROM test.t1;" > /tmp/f2
   val2=`tail -1 /tmp/f2`
 
